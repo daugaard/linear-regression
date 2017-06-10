21592ca1a2bb1cd9f0a2904f2a44feb2f1c33851
Soren Daugaard <sbd@ineptum.dk>
2017-06-10 11:10:44 -0400

    Fixes comment about data load

--- run.rb
+++ run.rb
@@ -4,8 +4,8 @@ require '.\linear_regression.rb'
 
 x_data = []
 y_data = []
-# Load data from CSV file into two dimensiolnal array
-# Each row contains square feet for living area and sale price like this: [ SQ FEET, SALE PRICE ]
+# Load data from CSV file into two arrays - one for independent variables X and one for the dependent variable Y
+# Each row contains square feet for property and living area like this: [ SQ FEET PROPERTY, SQ FEET HOUSE ]
 CSV.foreach("staten-island-single-family-home-sales-2015.csv", :headers => true) do |row|
   # use row here...
   x_data.push( [row[0].to_i, row[1].to_i] )
